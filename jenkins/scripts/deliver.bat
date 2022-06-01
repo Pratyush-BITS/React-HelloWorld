a20813aa1942bae02d01ea6e789b540f757f659c
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-06-02 00:12:49 +0530

    weird changes

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -3,10 +3,6 @@ echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/my-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
-set -x
-npm run build
-set +x
-
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
 echo 'The "npm start" command has a trailing ampersand so that the command runs'
@@ -15,6 +11,10 @@ echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+set -x
+npm run build
+set +x
 set -x
 npm start &
 sleep 1
