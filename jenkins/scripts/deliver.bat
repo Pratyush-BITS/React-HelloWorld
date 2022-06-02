e90ba4a3e760a7a45f9bad39f63472df430f519d
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-06-02 17:40:12 +0530

    updated deliver batch file

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,8 +1,7 @@
-bat npm run build
+npm run build
 npm install -g serve
-bat serve -s build
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+serve -s build
 
 echo $! > .pidfile
 
-echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
