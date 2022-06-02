5b7efa3da36b19055b725b4b37a29d6166c267fc
Pratyush-BITS <2021sp93070@wilp.bits-pilani.ac.in>
2022-06-02 09:06:02 +0530

    update kill process

--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -2,4 +2,6 @@ echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.bat"'
 echo 'was executed.'
 set -x
-kill $(cat .pidfile)
+@REM kill $(cat .pidfile)
+SET /P VALUE_FROM_FILE= < .pidfile
+taskkill /pid %VALUE_FROM_FILE% /f
